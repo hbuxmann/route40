bf99203fdbfa0e822e3c1ff500a5294f56a2425f
hernan <hbuxmann@gmail.com>
2022-07-17 22:07:09 -0300

    improves over release #2

--- components/ModalCustom/ModalCustom.jsx
+++ components/ModalCustom/ModalCustom.jsx
@@ -14,36 +14,60 @@ const ModalCustom = (props) => {
       transparent={true}
       visible={modalVisible}
     >        
-      <View style={styles.modal}>          
-        <View style={styles.modalTitle}>
-          <Text>Mi Modal</Text>
+      <View style={styles.modal}> 
+        <View  style={styles.modalView}>         
+          <View style={styles.modalTitle}>
+            <Text>Mi Modal</Text>
+          </View>
+          <View style={styles.modalMessage}>
+            <Text>Estas seguro que quieres borrar?</Text>
+          </View>
+          <View style={styles.modalMessage}>
+            <Text style={styles.modalItem}>{itemSelected.value}</Text>
+          </View>
+          <View style={styles.modalButton}>
+            <Button onPress={ () => onHandlerDeleteItem(itemSelected.id)} title='Confirmar' />
+            <Button onPress={ () => onHandlerCancel()} title='Cancelar' />
+          </View> 
         </View>
-        <View style={styles.modalMessage}>
-          <Text>Estas seguro que quieres borrar?</Text>
-        </View>
-        <View style={styles.modalMessage}>
-          <Text style={styles.modalItem}>{itemSelected.value}</Text>
-        </View>
-        <View style={styles.modalButton}>
-          <Button onPress={ () => onHandlerDeleteItem(itemSelected.id)} title='Confirmar' />
-          <Button onPress={ () => onHandlerCancel()} title='Cancelar' />
-        </View> 
       </View> 
     </Modal>
   )
 }
 
 const styles = StyleSheet.create({
-     modal : {
-      borderColor: 'black',
-      backgroundColor: 'white',
-      width: '80%',
-      height: '50%',
-      borderRadius: 10,
-      padding: '10%',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      flexDirection: 'column'
+    //  modal : {
+    //   borderColor: 'black',
+    //   backgroundColor: 'white',
+    //   width: '80%',
+    //   height: '50%',
+    //   borderRadius: 10,
+    //   padding: '10%',
+    //   justifyContent: 'space-between',
+    //   alignItems: 'center',
+    //   flexDirection: 'column'
+    // },
+
+    modal: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
     },
   
     modalTitle : {
